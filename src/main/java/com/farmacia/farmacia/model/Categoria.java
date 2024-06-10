5953c2f7c2c10e3c0dc4348ec9bed9bad70b374c
Thamires Cavalcante <thamires.araujo1996@gmail.com>
2024-06-10 16:41:59 -0300

    CRUD Produto

--- src/main/java/com/farmacia/farmacia/model/Categoria.java
+++ src/main/java/com/farmacia/farmacia/model/Categoria.java
@@ -1,10 +1,17 @@
 package com.farmacia.farmacia.model;
 
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -20,6 +27,20 @@ public class Categoria {
 	@NotBlank(message = "O Atributo Descrição é obrigatório")
 	@Size(min = 4, max = 500, message = "O campo descricao precisa ter entre 4 e 500 carcteres")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
+	
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 
 	public Long getId() {
 		return id;
